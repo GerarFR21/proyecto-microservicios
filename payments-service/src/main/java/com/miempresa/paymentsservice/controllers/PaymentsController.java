29a4a1ce8427033965ccd35939e09d8774155028
GerarFR21 <gerhardoadhonayflores@gmail.com>
2025-12-20 16:07:15 -0600

    usando Actuator

--- payments-service/src/main/java/com/miempresa/paymentsservice/controllers/PaymentsController.java
+++ payments-service/src/main/java/com/miempresa/paymentsservice/controllers/PaymentsController.java
@@ -1,13 +1,15 @@
 package com.miempresa.paymentsservice.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/payments")
-public class PaymentController {
+public class PaymentsController {
 
     @Value("${mensaje}")
     private String message;
